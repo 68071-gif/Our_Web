83876c2b409a3485d4b3414e06034f65ee0befe5
68071-gif <68071@students.riphah.edu.pk>
2026-03-24 12:03:36 +0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Firebase v8 config – keep logic same as your project
+var firebaseConfig = {
+  apiKey: "AIzaSyDeIbrHlZ7bm6INoxAz-l2xtHTodnmIfX8",
+  authDomain: "ourweb0101.firebaseapp.com",
+  projectId: "ourweb0101",
+  storageBucket: "ourweb0101.firebasestorage.app",
+  messagingSenderId: "815886778006",
+  appId: "1:815886778006:web:01a7691496b41ec931ccb7",
+  measurementId: "G-W8CEREXS4Y"
+};
+firebase.initializeApp(firebaseConfig);
+var auth = firebase.auth();
